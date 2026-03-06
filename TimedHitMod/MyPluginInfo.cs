2e969446ab6ce66a94a207bedaef2ba335e1ef2e
Max Barrows <max.f.barrows@gmail.com>
2026-03-06 15:09:44 -0800

    TimedHit

--- TimedHitMod/MyPluginInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace TimedHitMod;
-
-internal static class MyPluginInfo
-{
-    public const string PLUGIN_GUID    = "com.seaofstars.timedHitMod";
-    public const string PLUGIN_NAME    = "Timed Hit Mod";
-    public const string PLUGIN_VERSION = "1.0.0";
-}
